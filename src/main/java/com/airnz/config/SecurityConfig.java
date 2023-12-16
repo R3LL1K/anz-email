b98f4db03c9537b49860ab23817be51f87a555c2
Pranay <pnarotam@gmail.com>
2023-12-16 20:29:13 +1300

    enable basic security

---
+++ src/main/java/com/airnz/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.airnz.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+
+}

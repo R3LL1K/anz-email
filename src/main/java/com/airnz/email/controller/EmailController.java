504376b5f3b1fae9bd6b1ba67a9f18f5e20376a9
Pranay <pnarotam@gmail.com>
2023-12-17 07:31:33 +1300

    add the requestbody annotation to accept the email object as an input

--- src/main/java/com/airnz/email/controller/EmailController.java
+++ src/main/java/com/airnz/email/controller/EmailController.java
@@ -6,14 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.airnz.email.dto.InboxEmails;
+import com.airnz.email.dto.OutboxEmails;
 import com.airnz.email.service.InboxService;
+
+import lombok.extern.log4j.Log4j2;
 
 @RestController
 @RequestMapping("/v1/email")
+@Log4j2
 public class EmailController {
 
     @Autowired
@@ -29,7 +34,8 @@ public class EmailController {
     }
 
     @PostMapping(path = "/", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public InboxEmails writeAndSaveEmail(InboxEmails email) {
+    public OutboxEmails writeAndSaveEmail(@RequestBody OutboxEmails email) {
+        log.info("write and save email : ", email);
         return email;
     }
 

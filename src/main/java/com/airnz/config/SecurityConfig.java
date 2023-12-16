e7f3a7e815408caf6bf8dcd4d8a97a38479d16c8
Pranay <pnarotam@gmail.com>
2023-12-16 20:47:53 +1300

    making sure that basic auth uses the correct auth

--- src/main/java/com/airnz/config/SecurityConfig.java
+++ src/main/java/com/airnz/config/SecurityConfig.java
@@ -3,20 +3,47 @@ package com.airnz.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    private final BasicAuthenticationEntryPoint basicAuthenticationEntryPoint;
+
+    public SecurityConfig(BasicAuthenticationEntryPoint basicAuthenticationEntryPoint) {
+        this.basicAuthenticationEntryPoint = basicAuthenticationEntryPoint;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
-                .httpBasic();
+                .httpBasic()
+                .authenticationEntryPoint(basicAuthenticationEntryPoint);
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User
+                .withUsername("user")
+                .password("$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG")
+                .roles("USER_ROLE")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(8);
     }
 
 }
--- src/main/java/com/airnz/email/EmailApplication.java
+++ src/main/java/com/airnz/email/EmailApplication.java
@@ -2,12 +2,13 @@ package com.airnz.email;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.log4j.Log4j2;
 
-@SpringBootApplication
+@SpringBootApplication( exclude = { SecurityAutoConfiguration.class})
 @RestController
 @Log4j2
 public class EmailApplication {
